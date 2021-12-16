d3f5e0a8c3b9962f74ecc62df87f6a8e15e46edc
Jef Bucas <jfbucas@stp.dias.ie>
2021-12-16 21:33:25 +0000

    Scenarios

---
+++ scenarios/rowscan.py
@@ -0,0 +1,44 @@
+import scenario
+
+class RowScan( scenario.Scenario ):
+	"""The Heuristics with a simple rowscan Scenario"""
+
+	def __init__( self, puzzle ):
+
+		self.puzzle = puzzle
+		self.name = "rowscan"
+
+		self.score_target = 480
+		self.heuristic_patterns = [ 9, 12, 15 ]
+		self.heuristic_patterns_max_index = 160
+		self.conflicts_indexes_allowed = [256]
+
+		self.timelimit = 180 # Minutes
+
+		scenario.Scenario.__init__(self)
+
+	def prepare_patterns_count_heuristics( self ):
+
+		for i in range(self.puzzle.board_wh):
+			if i <= 16:
+				self.heuristic_patterns_count[i] = 0
+			elif i <= 26:
+				self.heuristic_patterns_count[i] = int((float(i) - 16) * float(2.8))
+			elif i <= 56:
+				self.heuristic_patterns_count[i] = int(((float(i) - 26) * float(1.43333)) + 28)
+			elif i <= 76:
+				self.heuristic_patterns_count[i] = int(((float(i) - 56) * float(0.9)) + 71)
+			elif i <= 102:
+				self.heuristic_patterns_count[i] = int(((float(i) - 76) * float(0.6538)) + 89)
+			elif i <= self.heuristic_patterns_max_index:
+				self.heuristic_patterns_count[i] = int(((float(i) - 102) / 4.4615) + 106)
+
+	def prepare_spaces_order( self ):
+
+		depth=0
+		for y in range(self.puzzle.board_h):
+			for x in range(self.puzzle.board_w):
+				s = x+y*self.puzzle.board_w
+				self.spaces_order[ s ] = depth 
+				depth+=1
+
