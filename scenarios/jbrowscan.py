a1650bbbd7b1e5a71c565722f7b6ea7d02b7c7df
Jef Bucas <jfbucas@stp.dias.ie>
2021-12-22 14:16:20 +0000

    jbrowscan2colors scenario

---
+++ scenarios/jbrowscan.py
@@ -0,0 +1,44 @@
+import scenario
+
+class RowScan( scenario.Scenario ):
+	"""The Heuristics with a simple rowscan Scenario"""
+
+	def __init__( self, puzzle ):
+
+		self.puzzle = puzzle
+		self.name = __name__.split(".")[1]
+
+		self.score_target = 480
+		self.heuristic_patterns = [ [ 9, 12, 15 ] ]
+		self.conflicts_indexes_allowed = [256]
+		self.heuristic_stats16 = False
+		self.depth_first_notification = 225
+
+		self.timelimit = 800 # Minutes
+
+		scenario.Scenario.__init__(self)
+
+	def prepare_patterns_count_heuristics( self ):
+
+		for i in range(self.puzzle.board_wh):
+			if i in range(17, 27):
+				self.heuristic_patterns_count[0][i] = int((i - 16) * 2.8)
+			elif i in range(27, 57):
+				self.heuristic_patterns_count[0][i] = int((i - 26) * 1.43333) + 28
+			elif i in range(57, 77):
+				self.heuristic_patterns_count[0][i] = int((i - 56) * 0.9) + 71
+			elif i in range(77, 103):
+				self.heuristic_patterns_count[0][i] = int((i - 76) * 0.6538) + 89
+			elif i in range(103, 161):
+				self.heuristic_patterns_count[0][i] = int((i - 102) / 4.4615) + 106
+
+	def prepare_spaces_order( self ):
+
+		depth=0
+		for y in range(self.puzzle.board_h):
+			for x in range(self.puzzle.board_w):
+				s = x+y*self.puzzle.board_w
+				self.spaces_order[ s ] = depth 
+				depth+=1
+
+scenario.global_list.append(RowScan)
---
+++ scenarios/jbrowscan2colors.py
@@ -0,0 +1,46 @@
+import scenario
+
+class JBRowScan2Colors( scenario.Scenario ):
+	"""The Heuristics with a simple rowscan Scenario"""
+
+	def __init__( self, puzzle ):
+
+		self.puzzle = puzzle
+		self.name = __name__.split(".")[1]
+
+		self.score_target = 480
+		self.heuristic_patterns = [ [ 9, 15 ] ]
+		self.conflicts_indexes_allowed = [256]
+		self.heuristic_stats16 = False
+		self.depth_first_notification = 226
+
+		self.timelimit = 800 # Minutes
+
+		scenario.Scenario.__init__(self)
+
+	def prepare_patterns_count_heuristics( self ):
+
+		for i in range(self.puzzle.board_wh):
+			if i in range(15, 27):
+				self.heuristic_patterns_count[0][i] = int((i - 14) * 2)
+			elif i in range(27, 37):
+				self.heuristic_patterns_count[0][i] = int((i - 26) * 1.3) + 24
+			elif i in range(37, 57):
+				self.heuristic_patterns_count[0][i] = int((i - 36) * 1.3) + 37
+			elif i in range(57, 77):
+				self.heuristic_patterns_count[0][i] = int((i - 56) * 0.85) + 57
+			elif i in range(77, 103):
+				self.heuristic_patterns_count[0][i] = int((i - 76) * 0.5) + 74
+			elif i in range(103, 151):
+				self.heuristic_patterns_count[0][i] = int((i - 102) / 5.3334) + 87
+
+	def prepare_spaces_order( self ):
+
+		depth=0
+		for y in range(self.puzzle.board_h):
+			for x in range(self.puzzle.board_w):
+				s = x+y*self.puzzle.board_w
+				self.spaces_order[ s ] = depth 
+				depth+=1
+
+scenario.global_list.append(JBRowScan2Colors)
